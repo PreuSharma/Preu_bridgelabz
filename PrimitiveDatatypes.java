1705eaecf540699c7e88b437dd271af3380cb08c
Preu Sharma <preu1061.be21@chitkara.edu.in>
2024-10-14 14:25:18 +0530

    fourth commit

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class PrimitiveDatatypes{
+	public static void main(String args[])  {
+		int a=41;
+		boolean b = false;
+		char c='c';
+		short d=5;
+		double e=4.55;
+		float f=6.7f;
+		long g=123456;
+		byte h=4;
+		System.out.println("integer: "+a);
+		System.out.println("boolean: "+b);
+		System.out.println("char: "+c);
+		System.out.println("short: "+d);
+		System.out.println("double: "+e);
+		System.out.println("float: "+f);
+		System.out.println("long: "+g);
+		System.out.println("byte: "+h);
+	}
+}
